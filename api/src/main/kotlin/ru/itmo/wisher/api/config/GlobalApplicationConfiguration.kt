c9b02f7ff3942cc1b56e47a4d226231d2dafb275
Fyodor Urlapov <urlapov-fyodor@mail.ru>
2024-11-03 13:03:42 +0300

    CORS enabled

--- api/src/main/kotlin/ru/itmo/wisher/api/config/GlobalApplicationConfiguration.kt
+++ api/src/main/kotlin/ru/itmo/wisher/api/config/GlobalApplicationConfiguration.kt
@@ -1,6 +1,5 @@
 package ru.itmo.wisher.api.config
 
-import jakarta.servlet.*
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -9,29 +8,17 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
+@EnableWebMvc
 class GlobalApplicationConfiguration {
     @Bean
     fun corsConfigurer() = object : WebMvcConfigurer {
         override fun addCorsMappings(registry: CorsRegistry) {
             registry
                 .addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
         }
-    }
-
-    @Bean
-    fun corsFilter() = object : Filter {
-        override fun init(filterConfig: FilterConfig?) {}
-
-        override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
-            val httpServletResponse = response as HttpServletResponse
-            httpServletResponse.setHeader("Access-Control-Allow-Origin", "*")
-            chain.doFilter(request, response)
-        }
-
-        override fun destroy() {}
     }
 }
