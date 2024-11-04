f82abffeb24b28f14a6f7c23c39e0f4ac0026130
Fyodor Urlapov <urlapov-fyodor@mail.ru>
2024-11-04 13:26:29 +0300

    CORS enabled

--- api/src/main/kotlin/ru/itmo/wisher/api/ApiApplication.kt
+++ api/src/main/kotlin/ru/itmo/wisher/api/ApiApplication.kt
@@ -2,28 +2,10 @@ package ru.itmo.wisher.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 class ApiApplication
 
 fun main(args: Array<String>) {
     runApplication<ApiApplication>(*args)
-}
-
-@Configuration
-class GlobalApplicationConfiguration {
-    @Bean
-    fun corsConfigurer() =
-        object : WebMvcConfigurer {
-            override fun addCorsMappings(registry: CorsRegistry) {
-                registry
-                    .addMapping("/**")
-                    .allowedOrigins("*")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE")
-            }
-        }
 }
--- api/src/main/kotlin/ru/itmo/wisher/api/example/presentation/PingController.kt
+++ api/src/main/kotlin/ru/itmo/wisher/api/example/presentation/PingController.kt
@@ -8,6 +8,7 @@ import ru.itmo.wisher.api.example.presentation.model.CreatePingPongRequest
 import ru.itmo.wisher.api.example.presentation.model.PingPong
 import java.util.UUID
 
+@CrossOrigin(exposedHeaders = ["*"])
 @RestController
 @RequestMapping("api/ping")
 class PingController(
