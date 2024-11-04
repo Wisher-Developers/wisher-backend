0e0409bccfaa7a1a7344942ae39a21f98952b367
Fyodor Urlapov <urlapov-fyodor@mail.ru>
2024-11-04 13:39:23 +0300

    CORS enabled

--- api/src/main/kotlin/ru/itmo/wisher/api/ApiApplication.kt
+++ api/src/main/kotlin/ru/itmo/wisher/api/ApiApplication.kt
@@ -2,10 +2,22 @@ package ru.itmo.wisher.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 class ApiApplication
 
 fun main(args: Array<String>) {
     runApplication<ApiApplication>(*args)
+}
+
+@Configuration
+@EnableWebMvc
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+    }
 }
