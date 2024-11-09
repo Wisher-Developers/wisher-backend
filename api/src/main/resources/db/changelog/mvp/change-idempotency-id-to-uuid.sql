7ee654c9be5da9e72e48e850624629f883971f79
Урлапов Фёдор Валерьевич <urlapov@skbkontur.ru>
2024-11-09 20:56:36 +0300

    Билдится и развертывается

--- api/src/main/resources/db/changelog/mvp/change-idempotency-id-to-uuid.sql
+++ api/src/main/resources/db/changelog/mvp/change-idempotency-id-to-uuid.sql
@@ -1,4 +1,6 @@
 --liquibase formatted sql
---changeset garnetaki:tables-init
+--changeset garnetaki:change-idempotency-id-to-uuid
 
-ALTER TABLE item ALTER COLUMN idempotency_id TYPE UUID;
+ALTER TABLE item DROP COLUMN idempotency_id;
+
+ALTER TABLE item ADD COLUMN idempotency_id UUID;
