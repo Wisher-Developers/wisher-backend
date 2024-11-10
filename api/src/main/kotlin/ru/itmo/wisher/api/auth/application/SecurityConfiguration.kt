f182d1852768b780d28039bb57f7382d7ff5656b
Урлапов Фёдор Валерьевич <urlapov@skbkontur.ru>
2024-11-10 21:25:29 +0300

    cors Гоняю

--- api/src/main/kotlin/ru/itmo/wisher/api/auth/application/SecurityConfiguration.kt
+++ api/src/main/kotlin/ru/itmo/wisher/api/auth/application/SecurityConfiguration.kt
@@ -16,6 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -55,15 +56,27 @@ class SecurityConfiguration : WebMvcConfigurer {
     }
 
     @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOriginPatterns = listOf("/**")
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        configuration.allowedHeaders = listOf("Authorization", "Content-Type")
+        configuration.allowCredentials = true
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+
+    @Bean
     fun securityFilterChain(
         http: HttpSecurity,
         jwtAuthenticationFilter: JwtAuthFilter,
         authenticationProvider: AuthenticationProvider,
+        corsConfigurationSource: CorsConfigurationSource,
     ): SecurityFilterChain {
-
-
         http
-            .cors { it.disable() }
+            .cors { it.configurationSource(corsConfigurationSource) }
             .csrf { it.disable() }
             .authorizeHttpRequests {
                 it.requestMatchers("/api/auth/**").permitAll()
