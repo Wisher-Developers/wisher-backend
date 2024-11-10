a6dd15c295bde28d687404641388806582853ebd
Урлапов Фёдор Валерьевич <urlapov@skbkontur.ru>
2024-11-10 22:54:12 +0300

    build

--- api/src/main/kotlin/ru/itmo/wisher/api/auth/application/SecurityConfiguration.kt
+++ api/src/main/kotlin/ru/itmo/wisher/api/auth/application/SecurityConfiguration.kt
@@ -15,9 +15,6 @@ import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import ru.itmo.wisher.api.user.application.UserRepository
 import ru.itmo.wisher.api.user.domain.exception.NoSuchUsername
