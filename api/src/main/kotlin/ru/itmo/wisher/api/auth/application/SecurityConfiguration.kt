601a966b9acdc0b2119aa92e5dd15c610a0fefef
Урлапов Фёдор Валерьевич <urlapov@skbkontur.ru>
2024-11-10 22:20:04 +0300

    ktlint

--- api/src/main/kotlin/ru/itmo/wisher/api/auth/application/SecurityConfiguration.kt
+++ api/src/main/kotlin/ru/itmo/wisher/api/auth/application/SecurityConfiguration.kt
@@ -18,7 +18,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import ru.itmo.wisher.api.user.application.UserRepository
 import ru.itmo.wisher.api.user.domain.exception.NoSuchUsername
